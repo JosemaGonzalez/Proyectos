d0b0c71b718461d04f83e1d8343030b9de3a78a0
JosemaGonzalez <josemagp94@gmail.com>
2017-04-29 11:47:44 +0200

    29042017

--- diw/portfolio/js/js.js
+++ diw/portfolio/js/js.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
 	let ventana_alto = $(window).height();
 	let ventana_alto2 = ventana_alto/4.3;
     $("main>div").height(ventana_alto);
@@ -6,7 +7,7 @@ $(document).ready(function() {
     $(".sobreizda,.sobredrcha").css("max-height", ventana_alto2/3);
     $(".sobreizda,.sobredrcha").css("min-height", ventana_alto2/3);
     $("#sobremi .izda,#sobremi .drcha").height(ventana_alto/2.8);
-    //$(".pro").height(auto);
+    $("#mistrabajos .izda a,#mistrabajos .drcha a").height(ventana_alto2/2);
     $('.svg').svgprogress({
         figure: "hexagon",
         progressFillGradient: ['#FAEB69','#5ECA50'],
@@ -39,23 +40,94 @@ $(document).ready(function() {
      }, 1000);
 
   });
-    if ($(window).width() > "768") {
-    	$(".oculto").css("display", "none");
-    	$("header ul").css("display", "block");
-    }
-    if ($(window).width() < "768") {
-    	$(".oculto").click(function(event) {
-    		event.preventDefault();
-    		$("header ul").fadeIn(500);
-    		$(".oculto").css("display", "none");
-    		$("header ul").css("display", "block");
-    	});
-    	$("main,li a").click(function(event) {
-    		$(".oculto").fadeIn(500);
-    		$(".oculto").css("display", "block");
-    		$("header ul").css("display", "none");
 
-    	});
+    if ($(window).height() < "550") {
+        $("#sobremi .izda,#sobremi .drcha").css("margin","1px auto");
+        $(".sobreizda,.sobredrcha").css("max-height", ventana_alto2/3.5);
+        $(".sobreizda,.sobredrcha").css("min-height", ventana_alto2/3.5);
+        $(".centro img").css("width", "40px");
+        $("#mistrabajos .izda,#mistrabajos .drcha").css("min-width","44.5%");
+        $("#mistrabajos .izda,#mistrabajos .drcha").css("margin","2px");
+        $("#mistrabajos .izda").css("float","left");
+        $("#mistrabajos .drcha").css("float","right");
+        $("#mistrabajos .izda a,#mistrabajos .drcha a").height(ventana_alto2/1.6);
+    }
+    if ($(window).height() < "550" || $(window).width() < "380") {
+        $(".apt img").css("width", "35px");
+        $(".apt img").css("margin", "0");
+        $(".apt div").css("width", "35px");
+        $(".apt div").css("height", "35px");
+        $(".apt div").css("top", "0");
+        $("#sobremi h4").css("margin-bottom", "3%");
+        $(".apt").css("margin-top", "0");
+        $(".apt").css("width", "33%");
+    }
+    if ($(window).width() > "768") {
+        $("#sobremi .izda,#sobremi .drcha").height(ventana_alto/3);
+        $(".sobreizda,.sobredrcha").css("max-height", ventana_alto2/3.2);
+        $(".sobreizda,.sobredrcha").css("min-height", ventana_alto2/3.2);
+    }
+
+    if ($(window).width() > "1024") {
+        $(".oculto").css("display", "none");
+        $("header ul").css("display", "block");
+        $("#rural a").hide("fast");
+        $("#rural").on("mouseover focus",function(){
+            $("#rural a").show("fast");
+        });
+        $("#rural").on("mouseleave",function(){
+            $("#rural a").hide("slow");
+        });
+        $("#hermandad a").hide("fast");
+        $("#hermandad").on("mouseover focus",function(){
+            $("#hermandad a").show("fast");
+        });
+        $("#hermandad").on("mouseleave",function(){
+            $("#hermandad a").hide("slow");
+        });
+        $("#salvador a").hide("fast");
+        $("#salvador").on("mouseover focus",function(){
+            $("#salvador a").show("fast");
+        });
+        $("#salvador").on("mouseleave",function(){
+            $("#salvador a").hide("slow");
+        });
+        $("#tareas a").hide("fast");
+        $("#tareas").on("mouseover focus",function(){
+            $("#tareas a").show("fast");
+        });
+        $("#tareas").on("mouseleave",function(){
+            $("#tareas a").hide("slow");
+        });
+        $("#sabio a").hide("fast");
+        $("#sabio").on("mouseover focus",function(){
+            $("#sabio a").show("fast");
+        });
+        $("#sabio").on("mouseleave",function(){
+            $("#sabio a").hide("slow");
+        });
+        $("#aceituna a").hide("fast");
+        $("#aceituna").on("mouseover focus",function(){
+            $("#aceituna a").show("fast");
+        });
+        $("#aceituna").on("mouseleave",function(){
+            $("#aceituna a").hide("slow");
+        });
+    }
+    if ($(window).width() < "1024") {
+
+        $(".oculto").click(function(event) {
+          event.preventDefault();
+          $("header ul").fadeIn(500);
+          $(".oculto").css("display", "none");
+          $("header ul").css("display", "block");
+      });
+        $("main,li a").click(function(event) {
+          $(".oculto").fadeIn(500);
+          $(".oculto").css("display", "block");
+          $("header ul").css("display", "none");
+
+      });
     }
 
 });
