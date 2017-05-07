c80dbfba3e804bad3a9cc63f71fed3f0a46afc6b
JosemaGonzalez <josemagp94@gmail.com>
2017-05-07 14:53:32 +0200

    07052017

--- dwec/ajax/ajaxjson/js/js.js
+++ dwec/ajax/ajaxjson/js/js.js
@@ -1,24 +1,35 @@
 {
-	$(document).ready(function() {
-		let botonA = $("#botonA");
-		let botonV =$("#botonV");
-		let botonJ = $("#botonJ");
+    $(document).ready(function () {
+        let botonA = $("#botonA");
+        let botonV = $("#botonV");
+        let botonJ = $("#botonJ");
+        let res = $("#resultado");
 
-		let pulsar = function(fichero){
-			$.getJSON( fichero , function( data ) {
-			});
-		}
+        let pulsar = function (fichero,objeto) {
+            $.getJSON(fichero, function (data) {
+                res.text("Tipo: "+ objeto.toUpperCase());
+                res.append("<ul>");
+                $.each(data[objeto], function (id, data2) {
+                    res.append("<li>" + data2["tipo"] + "</li>");
+                    if (objeto == "aceites") {
+                        res.append("<li>" + data2["descripcion"] + "</li>");
+                    }
 
-		botonA.click(function(){
-			pulsar("json/aceite.json");
-		});
-		botonV.click(function(){
-			pulsar("json/jamones.json");
+                });
+                res.append("</ul>");
+            });
+        }
 
-		});
-		botonJ.click(function(){
-			pulsar("json/vinos.json");
+        botonA.click(function () {
+            pulsar("json/aceite.json","aceites");
+        });
+        botonV.click(function () {
+            pulsar("json/jamones.json","jamones");
 
-		});
-	});
+        });
+        botonJ.click(function () {
+            pulsar("json/vinos.json","vinos");
+
+        });
+    });
 }
---
+++ dwec/ajax/ajaxjson/js/js2.js
@@ -0,0 +1,46 @@
+{
+    let botonA, botonJ, botonV;
+    let resultado;
+    let xhr;
+
+    let escribir = function (data, objeto) {
+        for (i in data) {
+            for (j in data[objeto]) {
+                resultado.innerHTML += data[objeto][j].tipo + "<br>";
+                if (objeto == "aceites")
+                    resultado.innerHTML += data[objeto][j].descripcion + "<br>";
+            }
+        }
+    }
+
+    let buscar = function (fichero, objeto) {
+        xhr = new XMLHttpRequest();
+        xhr.open("GET", fichero, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.status == 200 && xhr.readyState == 4) {
+                resultado.innerHTML = "";
+                escribir(JSON.parse(xhr.responseText), objeto);
+            }
+        }
+        xhr.send(null);
+    }
+
+    let init = function () {
+        botonA = document.getElementById("botonA");
+        botonJ = document.getElementById("botonJ");
+        botonV = document.getElementById("botonV");
+        resultado = document.getElementById("resultado");
+
+        botonJ.addEventListener("click", function () {
+            buscar("json/jamones.json", "jamones");
+        });
+        botonV.addEventListener("click", function () {
+            buscar("json/vinos.json", "vinos");
+        });
+        botonA.addEventListener("click", function () {
+            buscar("json/aceite.json", "aceites");
+        });
+    }
+
+    window.onload = init;
+}
